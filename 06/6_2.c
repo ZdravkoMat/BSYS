dbeeb8c605dbb3f8e76320ff8ec04b194bb3aac0
ZdravkoMat <zd.matic96@gmail.com>
2021-10-15 16:05:00 +0200

    6_2 V2

--- 06/6_2.c
+++ 06/6_2.c
@@ -4,19 +4,24 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/types.h>
-#include <sys/time.h>
 #include <sched.h>
+#include <time.h>
 
 int main()
 {
-    printf("hello world (pid:%d)\n", (int)getpid());
     cpu_set_t mask;
     CPU_ZERO(&mask);
-    int fd[2];
-
-    if (pipe(fd) == -1)
-    {
-        exit(1);
+	CPU_SET(0, &mask);
+    struct timespec start, end;
+    size_t tyield, loops = 1000000;
+    int first_pipefd[2], second_pipefd[2];
+    if (pipe(first_pipefd) == -1) {
+        perror("pipe");
+        exit(EXIT_FAILURE);
+    }
+    if (pipe(second_pipefd) == -1) {
+        perror("pipe");
+        exit(EXIT_FAILURE);
     }
 
     int rc = fork();
@@ -27,37 +32,29 @@ int main()
     }
     else if (rc == 0)
     {
-	    CPU_SET(1, &mask);
-        sched_setaffinity(0, sizeof(cpu_set_t), &mask);
-        close(fd[0]);
-        struct timeval t;
-        gettimeofday(&t, NULL);
-        write(fd[1], &t, sizeof(struct timeval));
-        exit(0);
+        if(sched_setaffinity(0, sizeof(cpu_set_t), &mask) == -1){
+            perror("sched_setaffinity");
+            exit(EXIT_FAILURE);
+        }
+        for (size_t i = 0; i < loops; i++) {
+            read(first_pipefd[0], NULL, 0);
+            write(second_pipefd[1], NULL, 0);
+        }
     }
     else
     {
-	    CPU_SET(1, &mask);
-        sched_setaffinity(0, sizeof(cpu_set_t), &mask);
-        close(fd[1]);
-        struct timeval t1;
-        read(fd[0], &t1, sizeof(struct timeval));
-        printf("t1.tv_sec = %ld, t1.tv_usec = %ld\n", t1.tv_sec, t1.tv_usec);
-
-        struct timeval t2;
-        gettimeofday(&t2, NULL);
-        printf("t2.tv_sec = %ld, t2.tv_usec = %ld\n", t2.tv_sec, t2.tv_usec);
-
-        long int sec_diff = t2.tv_sec - t1.tv_sec;
-        long int usec_diff = t2.tv_usec - t1.tv_usec;
-
-        if (usec_diff < 0)
-        {
-            sec_diff -= 1;
-            usec_diff = 1000000 + sec_diff;
+        if(sched_setaffinity(0, sizeof(cpu_set_t), &mask) == -1){
+            perror("sched_setaffinity");
+            exit(EXIT_FAILURE);
         }
-
-        printf("sec_diff = %ld, usec_diff = %ld\n", sec_diff, usec_diff);
+        clock_gettime(CLOCK_MONOTONIC, &start);
+        for(size_t i = 0; i < loops; i++){
+            write(first_pipefd[1], NULL, 0);
+            read(second_pipefd[0], NULL, 0);
+        }
+        clock_gettime(CLOCK_MONOTONIC, &end);
+        tyield = (end.tv_sec + end.tv_nsec - start.tv_sec + start.tv_nsec)/loops;
+        printf("%zu\n", tyield);
     }
     return 0;
 }
