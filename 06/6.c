2d2b648d7232c354dad1651868df57dd9e107532
ZdravkoMat <zd.matic96@gmail.com>
2021-10-18 15:20:20 +0200

    Chap 6 redone, chap 7,8,9

---
+++ 06/6.c
@@ -0,0 +1,68 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sched.h>
+#include <time.h>
+
+int main()
+{
+    cpu_set_t mask;
+    CPU_ZERO(&mask);
+    CPU_SET(1, &mask);
+    struct timespec start, end;
+    long int ytime, ftime, loops = 1000000;
+    int fd1[2], fd2[2];
+    if (pipe(fd1) == -1 || pipe(fd2) == -1)
+    {
+        perror("pipe");
+        exit(EXIT_FAILURE);
+    }
+
+    int rc = fork();
+    if (rc < 0)
+    {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    }
+    else if (rc == 0)
+    {
+        if (sched_setaffinity(0, sizeof(cpu_set_t), &mask) == -1)
+        {
+            perror("sched_setaffinity");
+            exit(EXIT_FAILURE);
+        }
+        for (int i = 0; i < loops; i++)
+        {
+            read(fd1[0], NULL, 0);
+            write(fd2[1], NULL, 0);
+        }
+    }
+    else
+    {
+        if (sched_setaffinity(0, sizeof(cpu_set_t), &mask) == -1)
+        {
+            perror("sched_setaffinity");
+            exit(EXIT_FAILURE);
+        }
+
+        clock_gettime(CLOCK_MONOTONIC, &start);
+        for (int i = 0; i < loops; i++)
+        {
+            write(fd1[1], NULL, 0);
+            read(fd2[0], NULL, 0);
+        }
+        clock_gettime(CLOCK_MONOTONIC, &end);
+        ytime = ((end.tv_sec + end.tv_nsec) - (start.tv_sec + start.tv_nsec)) / loops;
+        clock_gettime(CLOCK_MONOTONIC, &start);
+        for (int i = 0; i < loops; i++)
+        {
+        }
+        clock_gettime(CLOCK_MONOTONIC, &end);
+        ftime = ((end.tv_sec + end.tv_nsec) - (start.tv_sec + start.tv_nsec)) / loops;
+        printf("%ld\n", (ytime - ftime));
+    }
+    return 0;
+}
