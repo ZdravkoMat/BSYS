d9622a3d2d4397509c3864983573a62444bc5976
ZdravkoMat <zd.matic96@gmail.com>
2021-12-05 13:16:12 +0100

    coding chap. 27

--- 27/main-race.c
+++ 27/main-race.c
@@ -6,18 +6,18 @@ int balance = 0;
 pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 
 void* worker(void* arg) {
-    Pthread_mutex_lock(&lock);
+    pthread_mutex_lock(&lock);
     balance++; // unprotected access 
-    Pthread_mutex_unlock(&lock);
+    pthread_mutex_unlock(&lock);
     return NULL;
 }
 
 int main(int argc, char *argv[]) {
     pthread_t p;
     Pthread_create(&p, NULL, worker, NULL);
-    Pthread_mutex_lock(&lock);
+    pthread_mutex_lock(&lock);
     balance++; // unprotected access
-    Pthread_mutex_unlock(&lock);
+    pthread_mutex_unlock(&lock);
     Pthread_join(p, NULL);
     return 0;
 }
